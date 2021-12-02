5b7a5a6a38d149101a0e65d49df8be4cdf1c9623
MarceloDevAndroid <developermarcelosouza@gmail.com>
2021-12-02 11:56:42 -0300

    Configure initial Container NavHost setup

--- app/src/main/kotlin/com/marcelo/marvelapp/presentation/MainActivity.kt
+++ app/src/main/kotlin/com/marcelo/marvelapp/presentation/MainActivity.kt
@@ -1,12 +1,33 @@
 package com.marcelo.marvelapp.presentation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
 import com.marcelo.marvelapp.R
+import com.marcelo.marvelapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupNavHost()
+    }
+
+    private fun setupNavHost() {
+       val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.navHostContainer) as NavHostFragment
+
+        navController = navHostFragment.navController
+        appBarConfiguration = AppBarConfiguration(
+            setOf()
+        )
     }
 }
