6a9734dafc9ffb8a077e775a16482d9a8f64616e
MarceloDevAndroid <developermarcelosouza@gmail.com>
2021-12-07 10:37:27 -0300

    Map character endpoints. Configure core module dependencies.

---
+++ core/src/main/java/com/marcelo/core/data/response/CharacterResponse.kt
@@ -0,0 +1,7 @@
+package com.marcelo.core.data.response
+
+data class CharacterResponse(
+    val id: String,
+    val name: String,
+    val thumbnail: ThumbnailResponse
+)
---
+++ core/src/main/java/com/marcelo/core/data/response/ThumbnailResponse.kt
@@ -0,0 +1,6 @@
+package com.marcelo.core.data.response
+
+data class ThumbnailResponse(
+    val path: String,
+    val extension: String
+)
