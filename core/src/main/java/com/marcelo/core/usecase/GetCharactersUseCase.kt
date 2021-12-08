2928714010a30f16dd0982c7fc08ea888cb98f6d
MarceloDevAndroid <developermarcelosouza@gmail.com>
2021-12-07 22:19:44 -0300

    Configure Character UseCase

---
+++ core/src/main/java/com/marcelo/core/usecase/GetCharactersUseCase.kt
@@ -0,0 +1,22 @@
+package com.marcelo.core.usecase
+
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import com.marcelo.core.data.repository.interfaces.CharactersRepository
+import com.marcelo.core.domain.model.Character
+import com.marcelo.core.usecase.base.PagingUseCase
+import kotlinx.coroutines.flow.Flow
+
+class GetCharactersUseCase(
+    private val charactersRepository: CharactersRepository
+) : PagingUseCase<GetCharactersUseCase.GetCharactersParams, Character>() {
+
+    override fun createFlowObservable(params: GetCharactersParams): Flow<PagingData<Character>> {
+        return Pager(config = params.pagingConfig) {
+            charactersRepository.getCharacters(params.query)
+        }.flow
+    }
+
+    data class GetCharactersParams(val query: String, val pagingConfig: PagingConfig)
+}
